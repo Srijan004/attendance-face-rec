038af2200e28ba363828b5ba7f02d626721d10ca
Srijan004 <srijanrathor8@gmail.com>
2022-05-24 10:11:16 +0530

    making employee dashboard accessible  only to logged-in user

--- attendance-frontend/src/components/employeeDashboard/employee.js
+++ attendance-frontend/src/components/employeeDashboard/employee.js
@@ -4,63 +4,73 @@ import logo from "../../images/newLogoSmall.PNG";
 import entryIcon from "../../images/employeeEntering.png";
 import exitIcon from "../../images/employeeExiting.png";
 import viewProfile from "../../images/profileIcon.png";
+import Login from "../loginEmployee/login";
 
 const Employee = () => {
   const history = useHistory();
-
+  const loginCheck =  JSON.parse(localStorage.getItem("userDetail"));
+  
   return (
-    <div className="employeeDashboardFull">
-      <div className="employeeDashboardNavbar">
-        <img
-          src={logo}
-          alt=""
-          className="employeeDashboardProjectLogo"
-          onClick={() => history.push("/")}
-        />
-        <div
-          className="employeeLogoutOnDashboard"
-          onClick={() => {
-            localStorage.setItem("MyUser", JSON.stringify({}));
-            history.push("/");
-          }}
-        >
-          {" "}
-          Logout
-        </div>
-      </div>
-
-      <div className="employeeOptions">
-        <div
-          className="employeeDashboardOptions"
-          onClick={() => history.push("/markAttendance")}
-        >
-          <div className="imgEmpDashboard">
-            <img src={entryIcon} className="employeeDashboardImage" alt="" />
-          </div>
-
-          <div className="employeeDashboardLabel">Mark attendance In </div>
-        </div>
-        <div
-          className="employeeDashboardOptions"
-          onClick={() => history.push("/employeeProfile")}
-        >
-          <div className="imgEmpDashboard">
-            <img src={viewProfile} className="employeeDashboardImage" alt="" />
-          </div>
-          <div className="employeeDashboardLabel">View Profile</div>
-        </div>
-        <div
-          className="employeeDashboardOptions"
-          onClick={() => history.push("/markAttendanceOut")}
-        >
-          <div className="imgEmpDashboard">
-            <img src={exitIcon} className="employeeDashboardImage" alt="" />
-          </div>
-          <div className="employeeDashboardLabel">Mark attendance Out</div>
-        </div>
-      </div>
+    <>
+{
+  (loginCheck.empno) ?    <div className="employeeDashboardFull">
+  <div className="employeeDashboardNavbar">
+    <img
+      src={logo}
+      alt=""
+      className="employeeDashboardProjectLogo"
+      onClick={() => history.push("/")}
+    />
+    <div
+      className="employeeLogoutOnDashboard"
+      onClick={() => {
+        localStorage.setItem("MyUser", JSON.stringify({}));
+        localStorage.setItem("userDetail", JSON.stringify({}));
+        history.push("/");
+      }}
+    >
+      {" "}
+      Logout
     </div>
-  );
+  </div>
+
+  <div className="employeeOptions">
+    <div
+      className="employeeDashboardOptions"
+      onClick={() => history.push("/markAttendance")}
+    >
+      <div className="imgEmpDashboard">
+        <img src={entryIcon} className="employeeDashboardImage" alt="" />
+      </div>
+
+      <div className="employeeDashboardLabel">Mark attendance In </div>
+    </div>
+    <div
+      className="employeeDashboardOptions"
+      onClick={() => history.push("/employeeProfile")}
+    >
+      <div className="imgEmpDashboard">
+        <img src={viewProfile} className="employeeDashboardImage" alt="" />
+      </div>
+      <div className="employeeDashboardLabel">View Profile</div>
+    </div>
+    <div
+      className="employeeDashboardOptions"
+      onClick={() => history.push("/markAttendanceOut")}
+    >
+      <div className="imgEmpDashboard">
+        <img src={exitIcon} className="employeeDashboardImage" alt="" />
+      </div>
+      <div className="employeeDashboardLabel">Mark attendance Out</div>
+    </div>
+  </div>
+</div>
+ :
+ <Login /> 
+}
+    </>
+
+   );
 };
 
 export default Employee;
