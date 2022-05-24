dfcb64bafa5314423a75d67766f2b4eb1ee3ca5b
Srijan004 <srijanrathor8@gmail.com>
2022-05-24 09:40:41 +0530

    face matching score in percentage

--- attendance-backend/app.py
+++ attendance-backend/app.py
@@ -73,6 +73,16 @@ class Status(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+def face_distance_to_conf(face_distance, face_match_threshold=0.6):
+    if face_distance > face_match_threshold:
+        range = (1.0 - face_match_threshold)
+        linear_val = (1.0 - face_distance) / (range * 2.0)
+        return linear_val
+    else:
+        range = face_match_threshold
+        linear_val = 1.0 - (face_distance / (range * 2.0))
+        return linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
+
 
 time_duration=0
 def calculate_duration(inTimeVal,outTimeVal):
