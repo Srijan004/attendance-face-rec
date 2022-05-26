b961206cf026b3a3df12ce5f6fdf5f7ce9a85e4f
Srijan004 <srijanrathor8@gmail.com>
2022-05-26 18:17:53 +0530

    update mark-attendance-out and add css

--- attendance-frontend/src/components/markAttendanceOut/markAttendanceOut.js
+++ attendance-frontend/src/components/markAttendanceOut/markAttendanceOut.js
@@ -1,139 +1,96 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import Webcam from 'react-webcam';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import Webcam from "react-webcam";
+import { useHistory } from "react-router-dom";
 import logo from "../../images/newLogoSmall.PNG";
-import Login from '../loginEmployee/login';
+import Login from "../loginEmployee/login";
+import AccessDenied from "../AccessDenied/AccessDenied";
 
 const MarkAttendanceOut = () => {
-  const history = useHistory()
-  const loginCheck =  JSON.parse(localStorage.getItem("userDetail"));
+  const history = useHistory();
+  const loginCheck = JSON.parse(localStorage.getItem("userDetail"));
 
   const webcamRef = React.useRef(null);
   const videoConstraints = {
     width: 700,
     height: 400,
-    facingMode: 'user'
+    facingMode: "user",
   };
-  const[name, setName] = useState('')
-  const capture = React.useCallback(
-  () => {
-    
+  const [name, setName] = useState("");
+  const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
-    console.log(`imageSrc = ${imageSrc}`)
-                //for deployment, you should put your backend url / api
-    // axios.post('/markAttendance', {image : imageSrc, empno: localStorage.getItem("MyUser")})
-    // 	  .then(res => {
+    console.log(`imageSrc = ${imageSrc}`);
 
-    //       // history.push("/login");
-    //   	  // console.log(`response = ${res}`);
-    //       // localStorage.setItem("MyUser", JSON.stringify({}));
-    //       console.log( "markAtendnce flask says : ", res)
+    fetch("/markAttendanceOut", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        image: imageSrc,
+        empno: localStorage.getItem("MyUser"),
+      }),
+    }).then((res) =>
+      res.json().then((data) => {
 
-    //       // history.push("/login");
-    //   	  // setName(res.data)
-    // })
-    // 	  .catch(error => {
-    //   	  console.log(`error = ${error}`)
-    // })
-  
-    fetch("/markAttendanceOut",
-    {
-                'method':'POST',
-                 headers : {
-                'Content-Type':'application/json'
-          },
-          body:JSON.stringify( {image : imageSrc, empno: localStorage.getItem("MyUser")})
-        }
-    ).then((res) =>
-    res.json().then((data) => {
-        // Setting a data from api
-        // setdata({
-        //     name: data.Name,
-        //     age: data.Age,
-        //     date: data.Date,
-        //     programming: data.programming,
-        // });
+        console.log("Huo getUser resp : ", data);
 
-        console.log("Huo getUser resp : ", data)
-
-        if(data['login'] == 0) {
-            
-            alert("Please retry. Your attendance couldn't be made !!");
-            localStorage.setItem("MyUser", JSON.stringify({}));
-            history.push("/login");
-            
-        } 
-        
-        if(data['login'] == 2) {
-            alert("Mark your in attendance first. ");
-            // history.push("/employee");
-            
-        } 
-        
-        
-        
-        if(data['login'] == 1) {
-          alert('Attendce made');
-            // localStorage.setItem("MyUser", JSON.stringify({}));
-            // history.push("/employee");
+        if (data["login"] == 0) {
+          alert("Please retry. Your attendance couldn't be made !!");
+          localStorage.setItem("MyUser", JSON.stringify({}));
+          history.push("/login");
         }
 
+        if (data["login"] == 2) {
+          alert("Mark your in attendance first. ");
+        }
 
-    })
-);
+        if (data["login"] == 1) {
+          alert(`Attendce made !! You have been identified with ${data['percent_accuracy']}%`);
+        }
+      })
+    );
+  }, [webcamRef]);
 
-  
-  }, 
-   [webcamRef]
+  return (
+    <>
+      {loginCheck && loginCheck.empno ? (
+        <div className="webcamPageFull">
+          <div className="adminLoginNavbar">
+            <img
+              src={logo}
+              alt=""
+              className="adminProjectLogo"
+              onClick={() => history.push("/")}
+            />
+          </div>
+
+          <div className="webcamMain">
+            <div className="webcamInstruction">
+              <h2>
+                {" "}
+                Click on the button given below to Mark your Out-time attendance{" "}
+              </h2>
+            </div>
+
+            <Webcam
+              className="webcam"
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+
+            <button className="webcamButton" onClick={capture}>
+              Mark Attendance - Out
+            </button>
+          </div>
+        </div>
+      ) : (
+        <AccessDenied />
+      )}
+    </>
   );
-  
-  return ( 
-  <>
-  {
-    (loginCheck.empno) ? 
-
-    <div className="webcamPageFull">
-    <div className="adminLoginNavbar">
-      <img
-        src={logo}
-        alt=""
-        className="adminProjectLogo"
-        onClick={() => history.push("/")}
-      />
-    </div>
-  
-  <div className="webcamMain">
-  
-  <div className="webcamInstruction">
-  <h2> Click on the button given below to Mark your Out-time attendance </h2>
-  
-  </div>
-  
-    <Webcam
-      className="webcam"
-      audio={false}
-      ref={webcamRef}
-      screenshotFormat="image/jpeg"
-      videoConstraints={videoConstraints}
-    />
-  
-    <button className="webcamButton" onClick={capture}>Mark Attendance - Out</button>
-  
-  </div>
-  </div>
-
-:
-
-<Login />
-
-  }
-  
-  </>
-
-  
-	);
-  
 };
 
 export default MarkAttendanceOut;
