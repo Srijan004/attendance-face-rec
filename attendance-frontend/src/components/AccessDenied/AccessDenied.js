9c5d56d2a031e1c5bc27a53eaf495742d6133f1b
Srijan004 <srijanrathor8@gmail.com>
2022-05-27 15:41:11 +0530

    format frontend code

--- attendance-frontend/src/components/AccessDenied/AccessDenied.js
+++ attendance-frontend/src/components/AccessDenied/AccessDenied.js
@@ -1,24 +1,18 @@
 import accessDenied from "../../images/accessDenied.png";
-import './AccessDenied.css'
+import "./AccessDenied.css";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const AccessDenied = () => {
-    
-    const history = useHistory();
-    return ( 
-        
-        <div className="accessDenied">
+  const history = useHistory();
+  return (
+    <div className="accessDenied">
+      <img src={accessDenied} className="accessDeniedImg" alt="" srcset="" />
 
-<img src={accessDenied} className='accessDeniedImg' alt="" srcset="" />
+      <Link to="/">
+        <button className="reportButton">Go back to home page</button>
+      </Link>
+    </div>
+  );
+};
 
-<Link to="/">
-<button className="reportButton" >Go back to home page</button>
-</Link>
-        </div>
-
-
-
-     );
-}
- 
 export default AccessDenied;
